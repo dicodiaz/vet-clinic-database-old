1f2ca68d3c669128c7f33c6d11dc6ff883f9f3c2
Dico Diaz <dicodiaz@gmail.com>
2021-11-30 15:45:43 -0500

    Add species column, insert more data, commit transactions, rollback other transactions, write slightly more complex queries

--- data.sql
+++ data.sql
@@ -36,3 +36,161 @@ VALUES
     TRUE,
     5
   );
+
+-- Insert more data
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+VALUES
+  (
+    'Charmander',
+    TO_DATE('2020-02-08', 'YYYY-MM-DD'),
+    -11,
+    FALSE,
+    0
+  ),
+  (
+    'Plantmon',
+    TO_DATE('2022-11-15', 'YYYY-MM-DD'),
+    -5.7,
+    TRUE,
+    2
+  ),
+  (
+    'Squirtle',
+    TO_DATE('1993-04-02', 'YYYY-MM-DD'),
+    -12.13,
+    FALSE,
+    3
+  ),
+  (
+    'Angemon',
+    TO_DATE('2005-06-12', 'YYYY-MM-DD'),
+    -45,
+    TRUE,
+    1
+  ),
+  (
+    'Boarmon',
+    TO_DATE('2005-06-07', 'YYYY-MM-DD'),
+    20.4,
+    TRUE,
+    7
+  ),
+  (
+    'Blossom',
+    TO_DATE('1998-10-13', 'YYYY-MM-DD'),
+    17,
+    TRUE,
+    3
+  );
+
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before tranasction.
+BEGIN;
+
+UPDATE
+  animals
+SET
+  species = 'unspecified';
+
+SELECT
+  *
+FROM
+  animals;
+
+ROLLBACK;
+
+SELECT
+  *
+FROM
+  animals;
+
+/* Inside a transaction:
+ Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+ Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+ Commit the transaction.
+ Verify that change was made and persists after commit. */
+BEGIN;
+
+UPDATE
+  animals
+SET
+  species = 'digimon'
+WHERE
+  name LIKE '%mon';
+
+UPDATE
+  animals
+SET
+  species = 'pokemon'
+WHERE
+  species IS NULL;
+
+COMMIT;
+
+SELECT
+  *
+FROM
+  animals;
+
+ROLLBACK;
+
+SELECT
+  *
+FROM
+  animals;
+
+/* Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+ After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;) */
+BEGIN;
+
+TRUNCATE animals;
+
+SELECT
+  *
+FROM
+  animals;
+
+ROLLBACK;
+
+SELECT
+  *
+FROM
+  animals;
+
+/* Inside a transaction:
+ Delete all animals born after Jan 1st, 2022.
+ Create a savepoint for the transaction.
+ Update all animals' weight to be their weight multiplied by -1.
+ Rollback to the savepoint
+ Update all animals' weights that are negative to be their weight multiplied by -1.
+ Commit transaction */
+BEGIN;
+
+DELETE FROM
+  animals
+WHERE
+  date_of_birth >= TO_DATE('2022-01-01', 'YYYY-MM-DD');
+
+SAVEPOINT my_savepoint;
+
+UPDATE
+  animals
+SET
+  weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT my_savepoint;
+
+UPDATE
+  animals
+SET
+  weight_kg = weight_kg * -1
+WHERE
+  weight_kg < 0;
+
+COMMIT;
--- queries.sql
+++ queries.sql
@@ -33,7 +33,7 @@ FROM
 WHERE
   name IN ('Agumon', 'Pikachu');
 
--- List name and escape attempts of animals that weigh more than 10.5kg
+-- List name and escape attempts of animals that weigh more than 10.5kg.
 SELECT
   name,
   escape_attempts
@@ -58,7 +58,7 @@ FROM
 WHERE
   name != 'Gabumon';
 
--- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+-- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg).
 SELECT
   *
 FROM
@@ -66,3 +66,70 @@ FROM
 WHERE
   weight_kg BETWEEN 10.4
   AND 17.3;
+
+-- How many animals are there?
+-- 9
+SELECT
+  COUNT(*)
+FROM
+  animals;
+
+-- How many animals have never tried to escape?
+-- 2
+SELECT
+  COUNT(*)
+FROM
+  animals
+WHERE
+  escape_attempts = 0;
+
+-- What is the average weight of animals?
+-- 16.64kg
+SELECT
+  avg(weight_kg)
+FROM
+  animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+-- Neutered
+/* neutered | sum
+ ----------+-----
+ f        |   4
+ t        |  18 */
+SELECT
+  neutered,
+  SUM(escape_attempts)
+FROM
+  animals
+GROUP BY
+  neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+/* species | min | max
+ ---------+-----+-----
+ pokemon |  11 |  17
+ digimon |   8 |  45 */
+SELECT
+  species,
+  MIN(weight_kg),
+  MAX(weight_kg)
+FROM
+  animals
+GROUP BY
+  species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+-- 3 for pokemon, N/A for digimon since there is not a single digimon that satisfies the condition
+/* species |        avg
+ ---------+--------------------
+ pokemon | 3.0000000000000000 */
+SELECT
+  species,
+  AVG(escape_attempts)
+FROM
+  animals
+WHERE
+  date_of_birth BETWEEN TO_DATE('1990-01-01', 'YYYY-MM-DD')
+  AND TO_DATE('2000-12-31', 'YYYY-MM-DD')
+GROUP BY
+  species;
