a9d54f4d0bedf58d796f5483dafb32edc6fbda87
Dico Diaz <dicodiaz@gmail.com>
2021-12-07 14:21:19 -0500

    Add query to create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,4 +4,14 @@ CREATE TABLE patients (
     date_of_birth DATE NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT chk_date_of_birth CHECK (date_of_birth < CURRENT_DATE)
-)
+);
+
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at TIMESTAMP NOT NULL,
+    patient_id INT NOT NULL,
+    STATUS VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_medical_histories_patient_id FOREIGN KEY(patient_id) REFERENCES patients(id) ON DELETE CASCADE,
+    CONSTRAINT chk_medical_histories_admitted_at CHECK (admitted_at < CURRENT_TIMESTAMP)
+);
