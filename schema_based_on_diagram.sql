8edff44188294cd387b3bf3694e8bad161acfb1f
Dico Diaz <dicodiaz@gmail.com>
2021-12-07 14:46:54 -0500

    Add commands to create invoices and invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,7 +18,7 @@ CREATE TABLE medical_histories (
 
 CREATE TABLE treatments (
     id INT GENERATED ALWAYS AS IDENTITY,
-    type VARCHAR(250) NOT NULL,
+    TYPE VARCHAR(250) NOT NULL,
     name VARCHAR(250) NOT NULL,
     PRIMARY KEY(id)
 );
@@ -26,7 +26,35 @@ CREATE TABLE treatments (
 CREATE TABLE join_table_histories_treatments (
     medical_histories_id INT NOT NULL,
     treatments_id INT NOT NULL,
+    PRIMARY KEY(medical_histories_id, treatments_id),
     FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
     FOREIGN KEY (treatments_id) REFERENCES treatments(id)
 );
 
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    paid_at TIMESTAMP NOT NULL,
+    medical_history_id INT NOT NULL UNIQUE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    CONSTRAINT chk_invoices_total_amount CHECK (total_amount >= 0),
+    CONSTRAINT chk_invoices_generated_at CHECK (generated_at < CURRENT_TIMESTAMP),
+    CONSTRAINT chk_invoices_paid_at CHECK (paid_at < CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE invoice_items (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL NOT NULL,
+    invoice_id INT NOT NULL,
+    treatment_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+    CONSTRAINT chk_invoice_items_unit_price CHECK (unit_price >= 0),
+    CONSTRAINT chk_invoice_items_quantity CHECK (quantity >= 0),
+    CONSTRAINT chk_invoice_items_total_price CHECK (total_price >= 0)
+);
